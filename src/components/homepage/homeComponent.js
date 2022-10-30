ec7a6530ce7f493327227d6972c15e9589693c7d
Samuel Douay <douaysamuel@gmail.com>
2022-10-30 15:59:17 +0100

    init project

---
+++ src/components/homepage/homeComponent.js
@@ -0,0 +1,8 @@
+import { Component } from '../global';
+
+export class HomeComponent extends Component {
+	constructor() {
+		super('div', { name: 'id', value: 'homePage' }, '<h1>HOME PAGE</h1>');
+	}
+}
+	
